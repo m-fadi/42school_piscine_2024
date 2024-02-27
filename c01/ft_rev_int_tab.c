af96b10e1f1d94dfb11dea3e8075a231b6e0d292
fadi <fadi.m.marouf@gmail.com>
2024-02-27 23:10:26 +0100

    tab[i++]

--- c01/ft_rev_int_tab.c
+++ c01/ft_rev_int_tab.c
@@ -7,29 +7,24 @@
 void ft_rev_int_tab(int *tab, int size)
 {
      int temp = 0;
-     int i = 0;
-     int last = size - 1;
-     while (i < last)
+     int i = -1;
+     int last = size;
+     while (i++ < last--)
      {
           temp = tab[i];
           tab[i] = tab[last];
           tab[last] = temp;
-          i++;
-          last--;
      }
 }
 
 int main(void)
 {
-     int tab[] = {1, 2, 3, 4, 5, 6, 7};
-     int size = 7;
+     int tab[] = {1, 2, 3, 4, 5, 6};
+     int size = 6;
 
      ft_rev_int_tab(tab, size);
      int i = 0;
      while (i < size)
-     {
-          printf("%d", tab[i]);
-          i++;
-     }
+          printf("%d", tab[i++]);
      return (0);
 }
