d4761deaea36abcab2ecc5b7834377a9280c9db2
fadi <fadi.m.marouf@gmail.com>
2024-02-27 23:02:30 +0100

    *str++

--- c01/ft_strlen.c
+++ c01/ft_strlen.c
@@ -4,22 +4,18 @@
 // • Here’s how it should be prototyped :
 
 #include <unistd.h>
-    int
-    ft_strlen(char *str)
+int ft_strlen(char *str)
 {
      int i = 0;
      char c;
-     while (*str)
-     {
-          str++;
+     while (*str++)
           i++;
-     }
      return i;
 }
 
 int main(void)
 {
-     char *str = "helloo";
+     char *str = "helloo7";
      int l = ft_strlen(str) + '0';
      write(1, &l, 1);
      write(1, "\n", 1);
