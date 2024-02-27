13650e148ce8d85b59eb51cbb64c61e1846ac74a
fadi <fadi.m.marouf@gmail.com>
2024-02-27 23:41:53 +0100

    merge sort

--- c01/ft_sort_int_tab.c
+++ c01/ft_sort_int_tab.c
@@ -3,15 +3,17 @@
 // • Here’s how it should be prototyped :
 #include <stdio.h>
 #include <unistd.h>
+
+// merge sort
 void ft_sort_int_tab(int *tab, int size)
 {
-     
+     int mid = size / 2;
 }
 
 int main(void)
 {
      int tab[] = {0, 1, 2, 3, 4, 5, 6, 7};
-     int size = 7;
+     int size = sizeof(tab) / sizeof(tab[0]);
      int i = 0;
      ft_sort_int_tab(tab, size);
      while (size)
