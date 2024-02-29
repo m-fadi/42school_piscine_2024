d6beaf82e91ab3271c0af9299917feec3b0204d8
fadi <fadi.m.marouf@gmail.com>
2024-02-29 23:41:52 +0100

    ex04

---
+++ c02/ex04/ft_str_is_lowercase.c
@@ -0,0 +1,16 @@
+// Create a function that returns 1
+// if the string given as a parameter contains only
+//     lowercase alphabetical characters,
+//     and0 if it contains any other character.
+// • Here’s how it should be prototyped : ;
+// • It should return 1 if str is empty.
+
+int ft_str_is_lowercase(char *str)
+{
+     while (*str)
+          if (*str <= 'z' && *str >= 'a')
+               str++;
+          else
+               return (0);
+     return (1);
+}
