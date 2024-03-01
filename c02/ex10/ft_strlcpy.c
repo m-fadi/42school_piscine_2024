775299f86f2b784172cd170303bdbe7feb1b8780
fadi <fadi.m.marouf@gmail.com>
2024-03-01 16:42:26 +0100

    strlcpy

---
+++ c02/ex10/ft_strlcpy.c
@@ -0,0 +1,41 @@
+// Reproduce the behavior of the function strlcpy(man strlcpy).
+// • Here’s how it should be prototyped :
+
+int str_len(char *str)
+{
+     int i = 0;
+     while (*str)
+     {
+          i++;
+          str++;
+     }
+     return (i);
+}
+unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+     unsigned int n = size;
+     char *ptr = src;
+     while (*src && n - 1)
+     {
+          *dest = *src;
+          dest++;
+          src++;
+          n--;
+     }
+     *dest = '\0';
+     return (str_len(ptr));
+}
+// #include <bsd/string.h>
+// #include <stdio.h>
+// int main(void)
+// {
+//      char src[] = "HeLLO";
+//      char dest1[1];
+//      char dest2[1];
+
+//      // ft_strcapitalize(str);
+//      printf("%li\n", strlcpy(dest1, "Hello, World!", 1));
+//      printf("%s\n", dest1);
+//      printf("%i\n", ft_strlcpy(dest2, "Hello, World!", 1));
+//      printf("%s\n", dest2);
+// }
