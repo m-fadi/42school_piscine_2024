c506b6350e1ae6973f5f703fbd2e4477ba2a8e13
fadi <fadi.m.marouf@gmail.com>
2024-03-02 16:29:23 +0100

    str_cat

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -2,5 +2,11 @@
 // • Here’s how it should be prototyped :
 char *ft_strcat(char *dest, char *src)
 {
-     
+     char *ptr = dest;
+     while (*dest)
+          dest++;
+     while (*src)
+          *dest++ = *src++;
+     *dest = '\0';
+     return (ptr);
 }
