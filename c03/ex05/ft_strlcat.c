d6f18b3bbae48da5ca314b0cdeb5676c7759400f
fadi <fadi.m.marouf@gmail.com>
2024-03-03 10:04:57 +0100

    change the returned value

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -1,34 +1,41 @@
 // Reproduce the behavior of the function strlcat(man strlcat).
 // • Here’s how it should be prototyped : unsigned ;
-#include <stdio.h>
+// #include <stdio.h>
 int str_len(char *str)
 {
      int i = 0;
-     while (*str++)
+     while (*str)
+     {
           i++;
+          str++;
+     }
      return (i);
 }
-int ft_strlcat(char *dest, char *src, unsigned int size)
+unsigned int ft_strlcat(char *dest, char *src, unsigned int size)
 {
+     unsigned int dest_len = str_len(dest);
+     int src_len = str_len(src);
+     int total_len = dest_len + src_len;
+     unsigned int remaining_space = size > dest_len ? size - dest_len - 1 : 0;
 
-     int n = size;
-     while (*dest)
-          dest++;
-     while (*src && n--)
+     if (dest_len >= size)
+          return size + src_len; // If dest is already full, return required length without copying src.
+
+     dest += dest_len;
+     while (*src && remaining_space > 1)
      {
           *dest++ = *src++;
-          // n--;
+          remaining_space--;
      }
-     // while (*dest)
-     *dest++ = '\0';
-     return (str_len(dest) + str_len(src));
+     *dest = '\0';
+     return total_len;
 }
 
-int main()
-{
-     char *str = "345";
-     char dest[4] = "12";
-     ft_strlcat(dest, str, 2);
-     printf("%s", dest);
-     return 0;
-}
+// int main()
+// {
+//      char *str = "345";
+//      char dest[4] = "";
+//      ft_strlcat(dest, str, 2);
+//      printf("%s", dest);
+//      return 0;
+// }
