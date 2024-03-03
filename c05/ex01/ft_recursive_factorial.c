c7f8073c79f86616e175493cad374d281f87a524
fadi <fadi.m.marouf@gmail.com>
2024-03-03 15:13:47 +0100

    factorial

---
+++ c05/ex01/ft_recursive_factorial.c
@@ -0,0 +1,24 @@
+// Create a recursive function that returns
+// the factorial of the number given as a
+//     parameter.
+// • If the argument is not valid the function should return 0.
+// • Overflows must not be handled,
+//     the function return will be undefined.
+// • Here’s how it should be prototyped : ;
+
+int ft_recursive_factorial(int nb)
+{
+     int nb_fac = 1;
+     if (nb == 0)
+          return 0;
+     if (nb == 1)
+          return (1);
+     nb_fac *= nb * ft_recursive_factorial(nb - 1);
+     return (nb_fac);
+}
+
+// #include <stdio.h>
+// int main()
+// {
+//      printf("%d\n", ft_recursive_factorial(5));
+// }
