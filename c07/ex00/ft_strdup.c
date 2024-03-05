4a45f98616d322d78a3a12848c31247291119198
fadi <fadi.m.marouf@gmail.com>
2024-03-05 11:00:10 +0100

    strdup

--- c07/ex00/ft_strdup.c
+++ c07/ex00/ft_strdup.c
@@ -1,7 +1,27 @@
 // Reproduce the behavior of the function strdup(man strdup).
 // • Here’s how it should be prototyped :
-
+#include <string.h>
+#include <stdlib.h>
+// #include <stdio.h>
+int str_len(char *s)
+{
+     int i = 0;
+     while (*s++)
+          i++;
+     return (i);
+}
 char *ft_strdup(char *src)
 {
-     
+     char *dest = (char *)malloc(str_len(src) * sizeof(char));
+     char *ptr = dest;
+     while (*src)
+          *dest++ = *src++;
+     *dest = '\0';
+     return (ptr);
 }
+// #include <unistd.h>
+// int main(int ac, char **av)
+// {
+
+//      printf("%s", ft_strdup(av[1]));
+// }
