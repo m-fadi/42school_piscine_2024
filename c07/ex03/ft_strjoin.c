4e5f267ec0742e67fa8b61f4640a34735885ce0c
fadi <fadi.m.marouf@gmail.com>
2024-03-10 01:37:19 +0100

    join strs

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,70 +6,82 @@
 // • Here’s how it should be prototyped :;
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 int str_len(char *s)
 {
      int i = 0;
-     while (*s++)
+     while (s[i] != '\0')
           i++;
-     return (i);
+     return i;
 }
-char *ft_strcat(char *dest, char *src, char *sep)
-{
-     char *ptr_dest = dest;
 
-     while (*dest)
-          dest++;
-     while (*sep)
-          *dest++ = *sep++;
-
-     while (*src)
-          *dest++ = *src++;
-     return (ptr_dest);
-}
 char *ft_strjoin(int size, char **strs, char *sep)
 {
-     int len = 0;
-     while (*strs)
-          len += str_len(*strs++);
-     printf("%d", len);
-     // if (size <= 0)
-     //      return (NULL);
-     // char *conc_str = (char *)malloc(sizeof(char) * size);
-}
-int main()
-{
-     int main()
+     if (size == 0)
      {
-          // char *s1 = "123";
-          char *strs[] = {"456", "123"};
-          char *sep = ", ";
-          printf("Result of ft_strjoin: ");
-          for (int i = 0; i < 2; i++)
+          char *empty_string = (char *)malloc(sizeof(char));
+          *empty_string = '\0';
+          return empty_string;
+     }
+
+     int i = 0;
+     int total_length = 0;
+     int sep_length = str_len(sep);
+     {
+          total_length += str_len(strs[i]);
+          if (i < size - 1) // Add separator length for all but the last string
+               total_length += sep_length;
+          i++;
+     }
+
+   
+     char *conc_str = (char *)malloc(sizeof(char) * (total_length + 1));
+     if (conc_str == NULL)
+          return NULL; 
+
+     char *ptr_conc_str = conc_str;
+     i = 0;
+
+     // Copy strings and separators into the concatenated string
+     while (i < size)
+     {
+          char *ptr_str = strs[i];
+          while (*ptr_str)
           {
-               printf("%s", strs[i]); // Print each string in the array
-               if (i < 2 - 1)
+               *conc_str = *ptr_str;
+               ptr_str++;
+               conc_str++;
+          }
+          if (i < size - 1)
+          {
+               char *ptr_sep = sep;
+               while (*ptr_sep)
                {
-                    printf("%s", sep); // Print the separator except for the last string
+                    *conc_str = *ptr_sep;
+                    ptr_sep++;
+                    conc_str++;
                }
           }
-          printf("\n");
-
-          ft_strjoin(2, strs, sep);
+          i++;
      }
-     //      char *s1 = "123";
-     //      char s2[10] = "";
-     //      char *sep = ", ";
-     //      printf("%s", ft_strcat(s2, s1, sep));
+
+     *conc_str = '\0';    
+     return ptr_conc_str;
 }
 
-// #include <unistd.h>
-// int main(int ac, char **av)
+// int main()
 // {
-//      if (ac == 1)
-//           return (0);
-//      int i = 1;
-//      while (i < ac)
-//      {
-//           ft_strcat(av[1], av[2], ", ");
-//      }
+//      char *s1 = "1234";
+//      char *s2 = "4567";
+//      char *s3 = "7891";
+//      char *strs[] = {s1, s2, s3, NULL};
+//      char *sep = ", ";
+
+//      char *result = ft_strjoin(3, strs, sep); 
+//      printf("Concatenated string: %s\n", result);
+
+//      free(result);
+
+//      return 0;
 // }
