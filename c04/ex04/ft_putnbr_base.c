a2d727b39eb986117a4bb6f86a521eb0fd10b0a0
fadi <fadi.m.marouf@gmail.com>
2024-03-11 02:03:08 +0100

    modify ft_base

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -13,83 +13,104 @@
 // //  base contains the same character twice;
 // //  base contains + or -;
 // // • Here’s how it should be prototyped:
-// #include <unistd.h>
-// #include <stdio.h>
-// int str_len(char *s)
-// {
-//      int i = 0;
-//      while (*s++)
-//           i++;
-//      return (i);
-// }
-// int ft_strcmp(char *s1, char *s2)
-// {
-//      int i = 0;
-//      int s1_len = str_len(s1);
-//      int s2_len = str_len(s1);
-//      while (i < s1_len)
-//      {
-//           // printf("s1: %c\ns2: %c\n", *s1, *s2);
-//           // printf("i =  %d\n  s1 %c\ns2: %c\n", i, s1[i], s2[i]);
-//           i++;
-//      }
-//      // printf("i =  %d\n  s1 %c\ns2: %c\n", i, s1[i-1], s2[i-1]);
-//      // printf("i =  %d\n  s1 %c\ns2: %c\n", i, );
-//      return (s1[s1_len - 1] - s2[s2_len - 1]);
-// }
+#include <unistd.h>
+#include <stdio.h>
+void ft_putchar(char c)
+{
+     write(1, &c, 1);
+}
+int str_len(char *s)
+{
+     int i = 0;
+     while (*s++)
+          i++;
+     return (i);
+}
+int ft_strcmp(char *s1, char *s2)
+{
+     while (*s1 && *s2 && *s1 == *s2)
+     {
+          s1++;
+          s2++;
+     }
+     return (*s1 - *s2);
+}
 
-// int ft_base(char *str)
-// {
+int ft_base(char *str, char *base)
+{
+     // char **bases= {"01", "1234567", "0123456789", "0123456789ABCDEF"};
+     char *str_ptr = str;
+     // char *base_ptr = base;
+     // int i =0;
+     // while (i < 4)
+     // {
+     //      while (!ft_strcmp(*bases[i], base))
+     //           {
+     //                bases[i]++;
+     //                base++;
+     //           }
+     //           i++;
+     // }
+     // return (str_len(bases[i]));
 
-//      char *bs = "01";
-//      if (!ft_strcmp(str, bs))
-//           return (2);
-//      else if (!ft_strcmp(str, "12345678"))
-//           return (8);
-//      else if (!ft_strcmp(str, "0123456789"))
-//           return (9);
-//      else if (!ft_strcmp(str, "0123456789ABCDEF"))
-//           return (16);
 
-//      return (0);
-// }
-// void ft_putnbr_base(int nbr, char *base)
-// {
-//      int number = nbr;
-//      int i = 0;
-//      char converted_num[nbr / 5];
+         if (!ft_strcmp(str, "01"))
+         {
+              return (2);
+              str = str_ptr;
+         }
+         else if (!ft_strcmp(str, "01234567"))
+         {
+              return (8);
+              str = str_ptr;
+         }
+         else if (!ft_strcmp(str, "0123456789"))
+         {
+              return (10);
+              str = str_ptr;
+         }
+         else if (!ft_strcmp(str, "0123456789ABCDEF"))
+         {
+              return (16);
+              str = str_ptr;
+         }
 
-//      int convert_base = ft_base(base);
-//      // printf("%d", convert_base);
-//      //  if (!convert_base)
-//      //       return (0);
-//      //  if (convert_base == 16)
-//      //  {
-//      //  }
-//      // printf("number %d", number);
-//      while (i < nbr / 5)
-//      {
+         return (0);
+}
+void ft_putnbr_base(int nbr, char *base)
+{
+     int number = nbr;
+     int i = 0;
+     int size;
+     char *base_ptr = base;
+     int convert_base = ft_base(base, base);
+     size = (sizeof(int) * 8); // Calculate maximum digits required for an int
+     char converted_num[size];
+     converted_num[size] = '\0';
+     base = base_ptr;
 
-//           //
-//           // i++;
-//           // printf("%s", converted_num);
-          
-//           printf("number %d", number);
-//           // printf("convert_base: %s\n", base);
-//           // printf("number: %d\n", converted_num[i]);
-//           converted_num[i++] = base[number % convert_base];
-//           number = number / convert_base;
-//           // printf("index: %d\n", number % convert_base);
-//           // write(1, &base[number % convert_base], 1);
-//      }
-//      i = str_len(converted_num) - 1;
-//      // printf("%d\n", i);
-//      while (i >= 0)
-//           printf("%c", converted_num[i--]);
-// }
+     while (number != 0)
+     {
+          converted_num[i++] = base[number % convert_base];
+          number = number / convert_base;
+          if (number == 0)
+               break;
+     }
+     // Output the converted number
+     if (i == 0)
+     { // Handle the case when the input number is 0
+          ft_putchar('0');
+     }
+     else
+     {
+          while (i > 0)
+               ft_putchar(converted_num[--i]);
+     }
+}
 
-// int main()
-// {
-//      char *base = "012345678";
-//      ft_putnbr_base(100, base);
-// }
+int main()
+{
+     char *base = "0123456789ABCDEF";
+
+     ft_putnbr_base(15123, base);
+}
