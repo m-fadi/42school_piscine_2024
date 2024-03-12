0b6fd1c539384cd5007d39b494e0d52b602d0a19
fadi <fadi.m.marouf@gmail.com>
2024-03-12 12:12:31 +0100

    atoi_amaller than 10 base

--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -24,43 +24,6 @@ int str_len(char *s)
      return (i);
 }
 
-int ft_atoi(char *str)
-{
-     int number = 0;
-     int sign = 1;
-     char *str_ptr = str;
-     while (*str == ' ' || *str == '\t' || (*str > 9 && *str <= 13))
-          str++;
-     while (*str == '-' || *str == '+')
-     {
-          if (*str == '-')
-               sign *= -1;
-          str++;
-     }
-    // printf("%d", num)
-     //     str = str_ptr;
-     //      while ((*str <= '9' && *str >= '0' ))
-     //           number = number * 10+ *str++ - '0';
-     //      return (number * sign);
-     while ((*str >= '0' && *str <= '9') || (*str >= 'a' && *str <= 'f') || (*str >= 'A' && *str <= 'F'))
-     {
-          if (*str >= '0' && *str <= '9')
-          {
-               number = number * 16 + (*str - '0');
-          }
-          else if (*str >= 'a' && *str <= 'f')
-          {
-               number = number * 16 + (*str - 'a' + 10);
-          }
-          else if (*str >= 'A' && *str <= 'F')
-          {
-               number = number * 16 + (*str - 'A' + 10);
-          }
-          str++;
-     }
-     return (number*sign);
-}
-
 int base_is_valid(char *str)
 {
      int i = 1;
@@ -76,18 +39,62 @@ int base_is_valid(char *str)
      }
      return (1);
 }
+int atoi_smaller_10(char *str, int number, int base_len, char *base)
+{
+    
+     while ((*str <= base[base_len - 1] && *str >= base[0]))
+     {
+          number = number * base_len + *str++ - '0';
 
+     }
+     return (number);
+}
+int atoi_16(char *str, int number)
+{
+
+     while ((*str >= '0' && *str <= '9') || (*str >= 'A' && *str <= 'F') || (*str >= 'a' && *str <= 'f'))
+     {
+          if (*str >= '0' && *str <= '9')
+               number = number * 16 + (*str - '0');
+          else if (*str >= 'A' && *str <= 'F')
+               number = number * 16 + (*str - 'A' + 10);
+          else if (*str >= 'a' && *str <= 'f')
+               number = number * 16 + (*str - 'a' + 10);
+          str++;
+     }
+
+     return (number);
+}
 int ft_atoi_base(char *str, char *base)
 {
      if (!base_is_valid(base))
           return (0);
-     int num = ft_atoi(str);
-     // printf("%d", num);
-     return num;
+     int number = 0;
+     int sign = 1;
+     while (*str == ' ' || *str == '\t' || (*str > 9 && *str <= 13))
+          str++;
+     while (*str == '-' || *str == '+')
+     {
+          if (*str == '-')
+               sign *= -1;
+          str++;
+     }
+     int base_len = str_len(base);
+
+     if (base_len <= 10)
+          number = atoi_smaller_10(str, number, base_len, base);
+     else if (base_len == 16)
+     {
+
+          number = atoi_16(str, number);
+     }
+     return (number * sign);
 }
-int main()
-{
-     char *str = "  +---1242ABss";
-     char *base = "01234567";
-     printf("%x", ft_atoi(str));
-}
+// int main()
+// {
+//      char *str = "18";
+//      char *base = "01234567";
+//      int number = ft_atoi_base(str, base);
+//      // printf("%s\n", str);
+//      printf("%d\n", number);
+// }
