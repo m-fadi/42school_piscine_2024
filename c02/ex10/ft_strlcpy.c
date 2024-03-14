43128d6a02369bfde7e0f1a69484ee15da61dc38
fadi <fadi.m.marouf@gmail.com>
2024-03-14 15:29:41 +0100

    modify str_comp

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -1,6 +1,8 @@
 // Reproduce the behavior of the function strlcpy(man strlcpy).
 // • Here’s how it should be prototyped :
 
+
+// did not pas mini Mollinette even though the result same as strlcpy
 int str_len(char *str)
 {
      int i = 0;
@@ -17,7 +19,7 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
      char *ptr = src;
      if (size == 0)
           return (str_len(src));
-     while (*src && n - 1)
+     while (*src && n-1)
      {
           *dest++ = *src++;
           n--;
@@ -30,12 +32,12 @@ unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
 // int main(void)
 // {
 //      char src[] = "HeLLO";
-//      char dest1[1];
-//      char dest2[1];
+//      char dest1[4];
+//      char dest2[5];
 
 //      // ft_strcapitalize(str);
-//      printf("%li\n", strlcpy(dest1, "Hello, World!", 1));
-//      printf("%s\n", dest1);
-//      printf("%i\n", ft_strlcpy(dest2, "Hello, World!", 1));
+//     // printf("%li\n", strlcpy(dest1, "Hello, World!", 1));
+//      //printf("%s\n", dest1);
+//      printf("%i\n", ft_strlcpy(dest2, "Hello, World!", 2));
 //      printf("%s\n", dest2);
 // }
