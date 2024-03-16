e6eb379f240a770ff5ff0311192648aa8e776442
fadi <fadi.m.marouf@gmail.com>
2024-03-16 09:45:28 +0100

    main.c

---
+++ c08/ex01/main.c
@@ -0,0 +1,44 @@
+// Create a ft_boolean.h file.
+//  It’ll compile and run the following main appropriately:
+// #include "ft_boolean.h"
+// void ft_putstr(char *str)
+// {
+// while (*str)
+// write(1, str++, 1);
+// }
+// t_bool ft_is_even(int nbr)
+// {
+// return ((EVEN(nbr)) ? TRUE : FALSE);
+// }
+// int main(int argc, char **argv)
+// {
+// (void)argv;
+// if (ft_is_even(argc - 1) == TRUE)
+// ft_putstr(EVEN_MSG);
+// else
+// ft_putstr(ODD_MSG);
+// return (SUCCESS);
+// }
+// • This program should display
+// I have an even number of arguments.
+// • or
+// I have an odd number of arguments.
+#include "ft_boolean.h"
+void ft_putstr(char *str)
+{
+     while (*str)
+          write(1, str++, 1);
+}
+t_bool ft_is_even(int nbr)
+{
+     return ((EVEN(nbr)) ? TRUE : FALSE);
+}
+int main(int argc, char **argv)
+{
+     (void)argv;
+     if (ft_is_even(argc - 1) == TRUE)
+          ft_putstr(EVEN_MSG);
+     else
+          ft_putstr(ODD_MSG);
+     return (SUCCESS);
+}
