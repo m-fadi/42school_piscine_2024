99d503eebeee5b556ed17b5bf149db9d3aec04bb
fadi <fadi.m.marouf@gmail.com>
2024-03-16 14:04:54 +0100

    struct in header

---
+++ c08/ex03/ft_point.h
@@ -0,0 +1,23 @@
+// Create a file ft_point.h that’ll compile the following main:
+// #include "ft_point.h"
+// void set_point(t_point *point)
+// {
+// point->x = 42;
+// point->y = 21;
+// }
+// int main(void)
+// {
+// t_point point;
+// set_point(&point);
+// return (0);
+// }
+
+#ifndef ft_point
+#define ft_point
+
+typedef struct 
+{
+     int x;
+     int y;
+} t_point;
+#endif
