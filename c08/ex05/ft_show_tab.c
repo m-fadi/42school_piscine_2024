965cc22d23e1437c5ae9ce9e7f36735da8861456
fadi <fadi.m.marouf@gmail.com>
2024-03-22 16:38:16 +0100

    ss

--- c08/ex05/ft_show_tab.c
+++ c08/ex05/ft_show_tab.c
@@ -105,18 +105,15 @@ void ft_show_tab(t_stock_str *par)
           par++;
      }
 }
+libft_creator.sh, 
+                 // int main(int argc, char **argv)
+                 // {
+                 //      struct s_stock_str *arr = ft_strs_to_tab(argc, argv);
+                 //      if (arr == NULL)
+                 //           return 1;
+                 //      }
 
-// int main(int argc, char **argv)
-// {
-//      struct s_stock_str *arr = ft_strs_to_tab(argc, argv);
-//      if (arr == NULL)
-//           return 1;
-//      }
+                 //      ft_show_tab(arr);
 
-
-//      ft_show_tab(arr);
-
-
-
-//      return 0;
-// }
+                 //      return 0;
+                 // }
