bdd39b2e32cf510884c8be2e6cd987d6f569a873
fadi <fadi.m.marouf@gmail.com>
2024-03-27 16:01:06 +0100

    ي

--- c09/ex00/bft_creator.sh
+++ c09/ex00/bft_creator.sh
@@ -1,12 +1,5 @@
-# Create your ft library. It’ll be called libft.a.
-# • A shell script called libft_creator.sh will compile source files appropriately and
-# will create your library.
-# • This library should contain all of the following functions :
-# void ft_putchar(char c);
-# void ft_swap(int *a, int *b);
-# void ft_putstr(char *str);
-# int ft_strlen(char *str);
-# int ft_strcmp(char *s1, char *s2);
-# • We’ll launch the following command-line :
-# sh libft_creator.sh
+#!/bin/bash
+gcc -Wall -Wextra -Werror -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+ar rc libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+ranlib libft.a
 
