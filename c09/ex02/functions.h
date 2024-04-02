2b6c9b2c47a9b1b9ffb2b639a6f384e0a265ed10
fadi <fadi.m.marouf@gmail.com>
2024-04-03 00:46:23 +0200

    f

---
+++ c09/ex02/functions.h
@@ -0,0 +1,8 @@
+
+void ft_putchar(char c);
+void ft_strswap(char **s1, char **s2);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcomb(char *s1, char *s2);
+void ft_strsort(char **s, int count);
+
